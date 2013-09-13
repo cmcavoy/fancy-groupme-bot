fbdf2c0530ed5f4aa29e26be8f7646f34afd4d54
Chris McAvoy <chris.mcavoy@gmail.com>
2013-09-13 02:49:11 -0500

    can start up a simple bot, factored out the original bot. now works as an includable module.

---
+++ demo/repeatbot.js
@@ -0,0 +1,26 @@
+var bot = require('..');
+var util = require('util');
+
+// local configuration read from env.
+const TOKEN = process.env['TOKEN']; // your groupme api token
+const GROUP = process.env['GROUP']; // the room you want to join
+const NAME = process.env['NAME']; // the name of your bot
+const URL = process.env['URL']; // the domain you're serving from, should be accessible by Groupme.
+const CONFIG = {token:TOKEN, group:GROUP, name:NAME, url:URL};
+
+var mybot = bot(CONFIG);
+
+mybot.on('botRegistered', function(b) {
+  console.log("I am registered");
+  b.message("WHAT UP BRO?");
+});
+
+mybot.on('botMessage', function(b, message) {
+  console.log("I got a message, fyi");
+  if (message.name != b.name) {
+    b.message(message.name + " said " + message.text);
+  }
+});
+
+console.log("i am serving");
+mybot.serve(8000);
